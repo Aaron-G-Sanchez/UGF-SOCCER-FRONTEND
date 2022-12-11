7bb03f35174cdff604fa9c7929cf329853892875
Aaron Sanchez <saarong12@gmail.com>
2022-12-10 20:28:53 -0600

    Function set up to get id for league when div is clicked

--- src/components/LeagueDisplay.js
+++ src/components/LeagueDisplay.js
@@ -1,9 +1,13 @@
 const LeagueDisplay = ({ user, league }) => {
   // as a test this fucntion looks at the logged in user and the creator of the league to see if the ids are the same
-  if (user.id === league.leagues[0].creator_id) {
-    console.log(`${user.id} created this league`)
-  } else {
-    console.log('looking for creator')
+  // if (user.id === league.leagues[0].creator_id) {
+  //   console.log(`${user.id} created this league`)
+  // } else {
+  //   console.log('looking for creator')
+  // }
+
+  const getLeagueId = (id) => {
+    console.log(id)
   }
 
   return (
@@ -12,8 +16,12 @@ const LeagueDisplay = ({ user, league }) => {
         <h2 className="league-display-title">Leagues</h2>
         {league
           ? league.leagues.map((league) => (
-              <div className="league-card" key={league._id}>
-                <p>{league.name}</p>
+              <div
+                className="league-card"
+                key={league._id}
+                onClick={() => getLeagueId(league._id)}
+              >
+                <p className="league-name">{league.name}</p>
               </div>
             ))
           : null}
