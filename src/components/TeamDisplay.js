805ce1357b7431fa2a88f4466e0a95c9683a727a
Aaron Sanchez <saarong12@gmail.com>
2022-12-12 13:09:47 -0600

    Player shields are rendered to the team component

--- src/components/TeamDisplay.js
+++ src/components/TeamDisplay.js
@@ -3,6 +3,24 @@ const TeamDisplay = () => {
     <>
       <section className="team-display">
         <h1> Team info will show here</h1>
+        <div className="pitch">
+          <div className="forwards">
+            <div className="player-shield"></div>
+            <div className="player-shield"></div>
+            <div className="player-shield"></div>
+          </div>
+          <div className="midfield">
+            <div className="player-shield"></div>
+            <div className="player-shield"></div>
+            <div className="player-shield"></div>
+          </div>
+          <div className="defense">
+            <div className="player-shield"></div>
+            <div className="player-shield"></div>
+            <div className="player-shield"></div>
+            <div className="player-shield"></div>
+          </div>
+        </div>
       </section>
     </>
   )
