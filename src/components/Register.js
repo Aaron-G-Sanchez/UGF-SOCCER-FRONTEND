5800a8cb58fa77cab177bf3877a8375c1ef00f6d
Aaron Sanchez <saarong12@gmail.com>
2022-12-13 18:36:33 -0600

    Register component is styled and just needs the functionality

--- src/components/Register.js
+++ src/components/Register.js
@@ -17,49 +17,47 @@ const Register = ({ setToggle }) => {
   }
   return (
     <>
-      <div>
-        <section className="register-wrapper">
-          <h2 className="register-header">Register</h2>
-          <form className="form-body" onSubmit={handleSubmit}>
-            <input
-              onChange={handleChange}
-              className="email"
-              name="email"
-              type="email"
-              placeholder="Email"
-              value={formValues.email}
-            />
-            <input
-              onChange={handleChange}
-              className="username"
-              name="username"
-              type="text"
-              placeholder="Username"
-              value={formValues.username}
-            />
-            <input
-              onChange={handleChange}
-              className="password"
-              name="password"
-              type="password"
-              placeholder="Password"
-              value={formValues.password}
-            />
-            <input
-              onChange={handleChange}
-              className="password"
-              name="password"
-              type="password"
-              placeholder="Confirm password"
-              value={formValues.password}
-            />
+      <section className="register-wrapper">
+        <h2 className="register-header">Register</h2>
+        <form className="form-body" onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            className="email"
+            name="email"
+            type="email"
+            placeholder="Email"
+            value={formValues.email}
+          />
+          <input
+            onChange={handleChange}
+            className="username"
+            name="username"
+            type="text"
+            placeholder="Username"
+            value={formValues.username}
+          />
+          <input
+            onChange={handleChange}
+            className="password"
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={formValues.password}
+          />
+          <input
+            onChange={handleChange}
+            className="password"
+            name="password"
+            type="password"
+            placeholder="Confirm password"
+            value={formValues.password}
+          />
 
-            <button type="submit" className="register-submit">
-              Submit
-            </button>
-          </form>
-        </section>
-      </div>
+          <button type="submit" className="register-submit">
+            Submit
+          </button>
+        </form>
+      </section>
     </>
   )
 }
